12149b9e0ff95c80c78e442aaf7b8c8bc3e00a8f
Antonina Vlasova <b_tsets7@uni-bremen.de>
2026-03-21 16:39:25 +0100

    add a graph builder

---
+++ app/graph/graph_builder.py
@@ -0,0 +1,17 @@
+import networkx as nx
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+def build_dependency_graph(sentence: str):
+    doc = nlp(sentence)
+
+    G = nx.Graph()
+
+    for token in doc:
+        G.add_node(token.text, label=token.lemma_, pos=token.pos_)
+
+        if token.head != token:
+            G.add_edge(token.text, token.head.text, relation=token.dep_)
+
+    return G
