00aee27314d26fec2b8811caed6e84c8590394c8
Antonina Vlasova <b_tsets7@uni-bremen.de>
2026-03-21 15:13:09 +0100

    adjust main: add router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from app.api import upload
 
 app = FastAPI()
+app.include_router(upload.router, prefix="/api")
+
 @app.get("/")
 def root():
     return {"message": "Semantic Search API running"}
