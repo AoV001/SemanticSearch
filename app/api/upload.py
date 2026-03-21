4d2f30732016d68a61a42c6af5283e0751e68dc7
Antonina Vlasova <b_tsets7@uni-bremen.de>
2026-03-21 15:38:30 +0100

    adjust upload.py : return length of the text

--- app/api/upload.py
+++ app/api/upload.py
@@ -1,11 +1,24 @@
 from fastapi import APIRouter, File, UploadFile
 import os
 
+from app.services.file_service import read_txt_file
+
+# api to upload the txt. files
+
 router = APIRouter()
+
 UPLOAD_FOLDER = "data/"
+
 @router.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
+    if not file.filename.endswith(".txt"):
+        return {"error": "Only TXT Files are allowed"}
+
     file_location = os.path.join(UPLOAD_FOLDER, file.filename)
+
     with open(file_location, "wb+") as f:
         f.write(await file.read())
-    return {"filename": file.filename}
+
+    text = read_txt_file(file_location)
+
+    return {"filename": file.filename, "length": len(text)}
