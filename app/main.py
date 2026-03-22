87c8e893ad834d5be8e9337a450b3cf9528c897b
Antonina Vlasova <b_tsets7@uni-bremen.de>
2026-03-22 11:11:41 +0100

    adjust main: add router for search

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from app.api import upload
+from app.api import search
+
 
 app = FastAPI()
 app.include_router(upload.router, prefix="/api")
+app.include_router(search.router, prefix="/api")
 
 @app.get("/")
 def root():
