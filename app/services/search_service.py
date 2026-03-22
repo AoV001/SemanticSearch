ac0932cf324081fdecdb7576598210de6d658830
Antonina Vlasova <b_tsets7@uni-bremen.de>
2026-03-22 10:47:24 +0100

    add service for search the answer in the text for the input question

---
+++ app/services/search_service.py
@@ -0,0 +1,20 @@
+from app.graph.graph_builder import build_dependency_graph
+from app.graph.graph_similarity import graph_similarity
+from app.nlp.text_processing import split_sentences
+
+def search(question: str, text: str, top_k: int=3):
+
+    sentences = split_sentences(text)
+
+    question_graph = build_dependency_graph(question)
+
+    results = []
+
+    for sentence in sentences:
+
+        s_graph = build_dependency_graph(sentence)
+        score = graph_similarity(question_graph, s_graph)
+        results.append((sentence, score))
+
+    results.sort(key=lambda x: x[1], reverse=True)
+    return results[:top_k]
