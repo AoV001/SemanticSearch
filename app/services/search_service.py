b6a2ca7d3969235ea5705d3837bca2e1604a9ed0
Antonina Vlasova <b_tsets7@uni-bremen.de>
2026-03-22 12:19:55 +0100

    adjust search service due to the coreference and search by blocks of 3 sentences

--- app/services/search_service.py
+++ app/services/search_service.py
@@ -1,21 +1,38 @@
 from app.graph.graph_builder import build_dependency_graph
 from app.graph.graph_similarity import graph_similarity
 from app.nlp.text_processing import split_sentences
+from app.nlp.coreference import simple_coreference
+from spacy.lang.en import English
+
+WINDOW_SIZE = 3
+
+def split_blocks(sentences, window_size=WINDOW_SIZE):
+    blocks = []
+    for i in range(len(sentences) - window_size + 1):
+        block = " ".join(sentences[i:i+window_size])
+        blocks.append(block)
+    return blocks
 
 def search(question: str, text: str, top_k: int=3, threshold: float = 0.2):
 
-    sentences = split_sentences(text)
+    resolved_text = simple_coreference(text)
 
+    nlp_sent = English()
+    nlp_sent.add_pipe("sentencizer")
+    cleaned_text = resolved_text.replace("\n", " ")
+    doc = nlp_sent(cleaned_text)
+
+    sentences = [sent.text.strip() for sent in doc.sents]
+    blocks = split_blocks(sentences, window_size=WINDOW_SIZE)
     question_graph = build_dependency_graph(question)
 
     results = []
 
-    for sentence in sentences:
-
-        s_graph = build_dependency_graph(sentence)
-        score = graph_similarity(question_graph, s_graph)
+    for block in blocks:
+        block_graph = build_dependency_graph(block)
+        score = graph_similarity(question_graph, block_graph)
         if score >= threshold:
-            results.append((sentence, score))
+            results.append((block, score))
 
     results.sort(key=lambda x: x[1], reverse=True)
     return results[:top_k]
