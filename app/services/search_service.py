51fed256a133c611879ec9411ea7504c99e2143f
Antonina Vlasova <b_tsets7@uni-bremen.de>
2026-03-23 16:51:11 +0100

    adjust search service

--- app/services/search_service.py
+++ app/services/search_service.py
@@ -1,9 +1,9 @@
 from app.graph.graph_builder import build_dependency_graph
-from app.graph.graph_similarity import graph_similarity
 from app.nlp.text_processing import split_sentences
 from app.nlp.coreference import simple_coreference
 from spacy.lang.en import English
 from app.graph.graph_similarity import graph_similarity, extract_relevant_subgraph
+from app.nlp.answer_extraction import extract_answer
 
 
 WINDOW_SIZE = 3
@@ -15,7 +15,7 @@ def split_blocks(sentences, window_size=WINDOW_SIZE):
         blocks.append(block)
     return blocks
 
-def search(question: str, text: str, top_k: int=3, threshold: float = 0.2):
+def search(question, text, top_k=3, threshold=0.5):
     nlp_sent = English()
     nlp_sent.add_pipe("sentencizer")
     cleaned_text = text.replace("\n", " ")
@@ -23,7 +23,6 @@ def search(question: str, text: str, top_k: int=3, threshold: float = 0.2):
 
     sentences = [sent.text.strip() for sent in doc.sents]
     blocks = split_blocks(sentences, window_size=WINDOW_SIZE)
-
     resolved_blocks = [simple_coreference(block) for block in blocks]
 
     question_graph = build_dependency_graph(question)
@@ -34,7 +33,8 @@ def search(question: str, text: str, top_k: int=3, threshold: float = 0.2):
         score = graph_similarity(question_graph, block_graph)
         if score >= threshold:
             triplets = extract_relevant_subgraph(question_graph, block_graph, hop=1)
-            results.append((block, score, triplets))
+            answer = extract_answer(triplets, question_graph, block)  # <- передаём block
+            results.append((block, score, triplets, answer))
 
     results.sort(key=lambda x: x[1], reverse=True)
     return results[:top_k]
