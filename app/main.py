d07be6ddcd9780a209a38ebcbed5da0857f1e59c
Antonina Vlasova <b_tsets7@uni-bremen.de>
2026-03-26 14:10:18 +0100

    adjust main for frontend

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
-from app.api import upload
-from app.api import search
+from fastapi.middleware.cors import CORSMiddleware
+from app.api import upload, search, history
 from app.db.database import init_db
-from app.api import history
 
 
 app = FastAPI()
 init_db()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(upload.router, prefix="/api")
 app.include_router(search.router, prefix="/api")
 app.include_router(history.router, prefix="/api")
